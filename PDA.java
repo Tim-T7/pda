3a17e4b34e6cbe1f8910fce9e3a51b722a19b724
Timmy Tran <timmy.tran@mrjaffesclass.com>
2023-11-30 11:41:31 -0800

    Initial commit

---
+++ PDA.java
@@ -0,0 +1,42 @@
+/**
+ * Permissible Dating Age program
+ *
+ * @author Mr. Jaffe
+ * @version 2021-06-22 Version 1.0.0
+ */
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class PDA
+{
+
+    /**
+     * Constructor for objects of class PDA
+     */
+    public PDA()
+    {
+        // We don't need to do anything in the constructor for
+        // our program.
+    }
+    
+    /**
+     * This is the main event loop for our PDA program
+     */
+    public void runEventLoop() {
+    while (true) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("How old are you?");
+        int age = 0;
+        
+}
+   
+    }
+
+    /**
+     * The main method instantiates and runs the program
+     */
+    public static void main (String[] args) {
+        PDA pda = new PDA();
+        pda.runEventLoop();
+    }
+}
+
